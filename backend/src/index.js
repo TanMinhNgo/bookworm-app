e7336fad80290d99b153eeac183b25f5263c3386
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-09-14 03:09:32 +0700

    fix bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import 'dotenv/config';
 
 import authRoutes from './routes/authRoutes.js';
 import bookRoutes from './routes/bookRoutes.js';
+import { connect } from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT;
@@ -15,11 +16,12 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use('/api/books', bookRoutes);
 
+    connectDB();
+
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send("Connected to Db successfully" + connectDB().connection.host);
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-    connectDB();
 });
