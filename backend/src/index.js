690c764b88663c83a4310321b2678ae231f91fe8
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-09-14 03:15:59 +0700

    fix bug 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use('/api/books', bookRoutes);
 
-    connectDB();
+connectDB();
 
 app.get('/', (req, res) => {
     res.send("Connected to Db successfully" + connectDB().connection.host);
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,8 +2,10 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 
 const connectDB = async () => {
+    const dbURI = process.env.MONGODB_URL || "mongodb+srv://ngominhtan1252004_db_user:9RGrZkgj0rQSDK3v@cluster0.sf13o23.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URL);
+        const conn = await mongoose.connect(dbURI);
         console.log('MongoDB connected :', conn.connection.host);
     } catch (error) {
         console.error('MongoDB connection error:', error);
