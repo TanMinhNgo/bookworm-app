38954a7fdbe48a0d8beb8722c45c7c4056498218
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-09-14 03:18:53 +0700

    fix bug 3

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,12 +16,12 @@ app.use(cors());
 app.use("/api/auth", authRoutes);
 app.use('/api/books', bookRoutes);
 
-connectDB();
-
 app.get('/', (req, res) => {
-    res.send("Connected to Db successfully" + connectDB().connection.host);
+    connectDB();
+    res.send();
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    await connectDB();
 });
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,10 +2,8 @@ import mongoose from 'mongoose';
 import 'dotenv/config';
 
 const connectDB = async () => {
-    const dbURI = process.env.MONGODB_URL || "mongodb+srv://ngominhtan1252004_db_user:9RGrZkgj0rQSDK3v@cluster0.sf13o23.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
     try {
-        const conn = await mongoose.connect(dbURI);
+        const conn = await mongoose.connect(process.env.MONGODB_URL);
         console.log('MongoDB connected :', conn.connection.host);
     } catch (error) {
         console.error('MongoDB connection error:', error);
