dcad0f92cdd196aa4ac9ef462c170f012478205b
NgoMinhTan <ngominhtan1252004@gmail.com>
2025-09-14 05:01:44 +0700

    fix bug 4

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,10 +1,20 @@
+import { useAuthStore } from '@/store/authStore';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+type AuthStore = {
+    logout: () => void;
+}
 
 const Index = () => {
+    const {logout} = useAuthStore() as AuthStore;
+        
+
     return (
         <View>
-            
+            <TouchableOpacity onPress={logout}>
+                <Text>Logout</Text>
+            </TouchableOpacity>
         </View>
     );
 }
